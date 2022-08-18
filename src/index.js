1ce7e9742596a978729c070ea8fa5297594da771
azka1415 <rafifzaydan4@gmail.com>
2022-08-18 14:07:12 +0700

    deleted unused comments

--- src/index.js
+++ src/index.js
@@ -7,33 +7,6 @@ export const scrapeImages = async (username) => {
     const context = await browser.createIncognitoBrowserContext();
     const page = await context.newPage();
 
-
-    // await page.goto('https://www.instagram.com/')
-
-
-    // await page.waitForSelector('input')
-    // await page.screenshot({ path: 'loginPage.png' })
-
-    // await page.type('[name="username"]', 'azkarafifz')
-
-    // await page.screenshot({ path: 'nameTyped.png' })
-
-    // await page.type('[name="password"]', 'Azkarafif1415')
-
-    // await page.click('[type="submit"]')
-
-    // await page.screenshot({ path: 'loggedIn?.png' })
-
-    // const curUrl = await page.url()
-    // console.log(curUrl)
-
-    // await page.waitForSelector('img', {
-    //     visible: true
-    // })
-
-    // await page.screenshot({ path: 'whereNow.png' })
-    // await page.waitFor(5000)
-
     await page.goto(`https://www.instagram.com/${username}`, { waitUntil: 'networkidle0' })
 
     await page.waitForSelector('img', {
